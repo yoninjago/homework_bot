1460694a212bcced2dd37916c20310c308d982b4
Sergey Troshin <nebazist@gmail.com>
2022-02-14 23:43:27 +0500

    Fixed exceptions and logging

--- homework.py
+++ homework.py
@@ -2,6 +2,7 @@ import logging
 import os
 import sys
 import time
+from logging.handlers import RotatingFileHandler
 
 import requests
 import telegram
@@ -16,38 +17,51 @@ TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+LOG_FILENAME = __file__ + '.log'
 
-HOMEWORK_STATUSES = {
+HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
-MESSAGE = 'Бот отправил сообщение: {message}'
+MESSAGE = 'Отправка сообщения в чат: {message}'
 RESPONSE_ERROR = ('Эндпоинт {endpoint} недоступен.'
-                  'Код ответа API: {status_code}')
-UNEXPECTED_RESPONSE = 'Неожиданный ответ сервера: {error}'
-RESPONSE_MISSING_KEYS = ('Домашняя работа не содержит необходимые поля.'
-                         'Имя работы: {homework_name}'
-                         'Статус: {homework_status}')
+                  'Код ответа API: {status_code}'
+                  ' Используемые параметры запроса:'
+                  ' url: {url}; headers: {headers}; params: {params}')
+CONNECTION_ERROR = ('Проблемы с подключением к серверу: {error}.'
+                    ' Используемые параметры запроса:'
+                    ' url: {url}; headers: {headers}; params: {params}')
+UNEXPECTED_RESPONSE = ('Неожиданный ответ сервера. Описание проблемы: {error}.'
+                       ' Код ответа API: {status_code}.'
+                       ' Используемые параметры запроса:'
+                       ' url: {url}; headers: {headers}; params: {params}.')
 UNEXPECTED_RESPONSE_TYPE = 'Неожиданный формат данных ответа: {type}'
 UNEXPECTED_HOMEWORK_TYPE = ('Неожиданный формат данных '
                             'домашнего задания: {type}')
 UNEXPECTED_STATUS = 'Неожиданный статус домашней работы: {status}'
 HOMEWORK_STATUS_CHANGE = ('Изменился статус проверки работы "{homework_name}".'
                           ' {verdict}')
-ENVIRONMENT_VARIABLES_MISSING = ('Отсутствуют переменные окружения. '
-                                 'Работа программы прекращена')
+ENVIRONMENT_VARIABLES_MISSING = (
+    'Отсутствует обязательная переменная окружения: {name}')
+STOP_BOT = 'Программа принудительно остановлена.'
 STATUS_NOT_CHANGE = 'В ответе отсутствуют новые статусы домашней работы'
 ERROR_MESSAGE = 'Сбой в работе программы: {error}'
 
-
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-handler = logging.StreamHandler(sys.stdout)
 formatter = logging.Formatter('%(asctime)s [%(levelname)s] Event: %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
+file_handler = RotatingFileHandler(
+    LOG_FILENAME, maxBytes=1000000, backupCount=2
+)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 
 def send_message(bot, message):
@@ -58,25 +72,35 @@ def send_message(bot, message):
 
 def get_api_answer(current_timestamp):
     """Запрос к эндпоинту API-сервиса."""
-    timestamp = current_timestamp or int(time.time())
+    REQUEST = {
+        'url': ENDPOINT,
+        'headers': HEADERS,
+        'params': {'from_date': current_timestamp}
+    }
     try:
-        homework_statuses = requests.get(
-            ENDPOINT, headers=HEADERS, params={'from_date': timestamp}
-        )
-        if not homework_statuses.status_code // 100 == 2:
-            raise ValueError(RESPONSE_ERROR.format(
-                endpoint=ENDPOINT, status_code=homework_statuses.status_code
+        homework_statuses_json = requests.get(**REQUEST)
+    except requests.exceptions.ConnectionError as error:
+        raise ConnectionError(CONNECTION_ERROR.format(**REQUEST, error=error))
+    homework_statuses = homework_statuses_json.json()
+    for field in ['error', 'code']:
+        if field in homework_statuses:
+            raise RuntimeError(UNEXPECTED_RESPONSE.format(
+                **REQUEST,
+                error=homework_statuses.get(field),
+                status_code=homework_statuses_json.status_code
             ))
-    except requests.exceptions.RequestException as error:
-        raise ValueError(UNEXPECTED_RESPONSE.format(error=error))
-    return homework_statuses.json()
+    if homework_statuses_json.status_code != 200:
+        raise ValueError(RESPONSE_ERROR.format(
+            endpoint=ENDPOINT, status_code=homework_statuses_json.status_code
+        ))
+    return homework_statuses
 
 
 def check_response(response):
     """Проверка ответа API на корректность."""
-    if type(response) != dict:
+    if not isinstance(response, dict):
         raise TypeError(UNEXPECTED_RESPONSE_TYPE.format(type=type(response)))
-    if type(response.get('homeworks')) != list:
+    if not isinstance(response.get('homeworks'), list):
         raise TypeError(UNEXPECTED_HOMEWORK_TYPE.format(
             type=type(response.get('homeworks'))
         ))
@@ -85,24 +109,20 @@ def check_response(response):
 
 def parse_status(homework):
     """Извлекает статус домашней работы работы."""
-    homework_name = homework.get('homework_name')
-    homework_status = homework.get('status')
-    if homework_name is None or homework_status is None:
-        raise KeyError(RESPONSE_MISSING_KEYS.format(
-            homework_name=homework_name, homework_status=homework_status
-        ))
-    verdict = HOMEWORK_STATUSES.get(homework_status)
-    if not verdict:
-        raise KeyError(UNEXPECTED_STATUS.format(status=homework_status))
+    homework_name = homework['homework_name']
+    status = homework['status']
+    if status not in HOMEWORK_VERDICTS:
+        raise ValueError(UNEXPECTED_STATUS.format(status=status))
     return HOMEWORK_STATUS_CHANGE.format(
-        homework_name=homework_name, verdict=verdict
+        homework_name=homework_name, verdict=HOMEWORK_VERDICTS[status]
     )
 
 
 def check_tokens():
     """Проверяет доступность необходимых переменных окружения."""
-    for token in [PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID]:
-        if not token:
+    for name in ['PRACTICUM_TOKEN', 'TELEGRAM_TOKEN', 'TELEGRAM_CHAT_ID']:
+        if not globals()[name]:
+            logger.critical(ENVIRONMENT_VARIABLES_MISSING.format(name=name))
             return False
     return True
 
@@ -110,30 +130,31 @@ def check_tokens():
 def main():
     """Основная логика работы бота."""
     if not check_tokens():
-        return logger.critical(ENVIRONMENT_VARIABLES_MISSING)
+        raise ValueError(STOP_BOT)
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
-    homework = []
-    count_error = 0
+    sent_message = ''
     while True:
         try:
             response = get_api_answer(current_timestamp)
-            new_homework = check_response(response)
-            if new_homework != homework and new_homework:
-                send_message(bot, parse_status(new_homework[0]))
-                homework = new_homework
+            new_homeworks = check_response(response)
+            if new_homeworks:
+                send_message(bot, parse_status(new_homeworks[0]))
             else:
                 logger.debug(STATUS_NOT_CHANGE)
+            time.sleep(RETRY_TIME)
         except Exception as error:
-            logger.error(error)
-            if count_error == 0:
-                send_message(bot, ERROR_MESSAGE.format(error=error))
-                count_error += 1
+            message = ERROR_MESSAGE.format(error=error)
+            logger.exception(message)
+            if sent_message != message:
+                sent_message = message
+                try:
+                    send_message(bot, message)
+                except Exception as error:
+                    logger.exception(ERROR_MESSAGE.format(error=error))
             time.sleep(RETRY_TIME)
         else:
-            count_error = 0
-            current_timestamp = response.get('current_date')
-            time.sleep(RETRY_TIME)
+            current_timestamp = response.get('current_date', current_timestamp)
 
 
 if __name__ == '__main__':
